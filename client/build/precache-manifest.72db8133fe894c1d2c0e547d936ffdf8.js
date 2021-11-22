dd3bff1bbdfe2a8cead6394e49908686b06de442
Missy Stiner <missystiner@gmail.com>
2021-11-21 23:05:59 -0600

    serviceworker troubleshooting

---
+++ client/build/precache-manifest.72db8133fe894c1d2c0e547d936ffdf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e9374f0e9c17b84c6cf555d899d0d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f2c1e49426d3c57206",
+    "url": "/static/css/main.d1506c43.chunk.css"
+  },
+  {
+    "revision": "1921a88635dcfeeed715",
+    "url": "/static/js/2.9a8eb586.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.9a8eb586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f2c1e49426d3c57206",
+    "url": "/static/js/main.3a2bb471.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
